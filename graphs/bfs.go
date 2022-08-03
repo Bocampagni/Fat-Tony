f7cadcc8d260579c4411daed08e1d8ec2c4af640
bocampagni <a.bocampagni@gmail.com>
2022-08-03 15:04:21 -0300

    implemented bfs

--- graphs/bfs.go
+++ graphs/bfs.go
@@ -1,5 +1,47 @@
 package main
 
-func Bfs(graph Graph) *Graph {
-	return new(Graph)
+import (
+	"container/list"
+	"fmt"
+)
+
+func Bfs(graph *Graph) {
+
+	for i := 0; i < len(graph.V); i++ {
+		graph.V[i].color = 0
+		graph.V[i].parent = -1
+		graph.V[i].id = i
+	}
+
+	graph.V[0].color = 1
+	time := 0
+	queue := list.New()
+	queue.PushFront(graph.V[0].id)
+
+	for {
+		head := queue.Front()
+		vertex := graph.V[head.Value.(int)]
+		if vertex.color == 1 {
+			BfsVisit(graph, &vertex, &time, queue)
+			queue.Remove(head)
+			time += 1
+		}
+		if queue.Len() == 0 {
+			break
+		}
+	}
+
+	fmt.Printf("%#v", *graph)
+}
+
+func BfsVisit(graph *Graph, node *node, time *int, queue *list.List) {
+	for _, element := range graph.E[node.id] {
+		if element.color == 0 {
+			element.color = 1
+			element.parent = node.id
+			element.time = *time
+			queue.PushFront(element.id)
+		}
+	}
+	graph.V[node.id].color = 2
 }
