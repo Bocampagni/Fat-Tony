ca9b77f317664c273d5b83e84c69cefe8e6b2e4b
bocampagni <a.bocampagni@gmail.com>
2022-09-29 11:22:28 -0300

    adding final solution

--- artificialIntelligence/jarras.pl
+++ artificialIntelligence/jarras.pl
@@ -1,7 +1,7 @@
-objetivo(2,0).
-objetivo(2,1).
-objetivo(2,2).
-objetivo(2,3).
+objetivo((2,0)).
+objetivo((2,1)).
+objetivo((2,2)).
+objetivo((2,3)).
 
 acao((J1,J2),encher1,(4,J2)):- J1<4.
 acao((J1,J2),encher2,(J1,3)):- J2<3.
@@ -18,3 +18,41 @@ acao((J1,J2),passar21,(J1FINAL,0)):- J2=<4-J1,
 
 vizinhos(N,FilhosN):-
     findall(Y,acao(N,_,Y),FilhosN).
+
+enqueue(NN,F1,F2):- append(F1,NN,F2).
+stackin(NN,F1,F2):- append(NN,F1,F2).
+is_goal(Node):- objetivo(Node).
+
+% % Base
+% search([Node|_]):- is_goal(Node).
+% search([Node|F1]):- vizinhos(Node, NN),
+%     				enqueue(NN,F1,F2),
+%     				search(F2).
+
+% % Com lista de visitados
+% search([Node|_],_):- is_goal(Node).
+% search([Node|F1],Visited):- vizinhos(Node, NN),
+%         			enqueue([Node],Visited,F3),
+%     				enqueue(NN,F1,F2),
+%     				search(F2,F3).
+
+sanitize(L,[],L).
+sanitize(NN,[H|T],L):-
+    delete(NN,H,NewNN),
+    sanitize(NewNN,T,L).
+
+%Sem repetição de estados visitados.
+search([Node|_],_):- is_goal(Node).
+search([Node|F1],Visited):- vizinhos(Node, NN),
+    				enqueue([Node],Visited,F3),
+    				sanitize(NN,F3,SanitizedNN),
+				    enqueue(SanitizedNN,F1,F2),
+    				search(F2,F3).
+
+% % Busca em profundidade
+% search([Node|_],_):- is_goal(Node).
+% search([Node|F1],Visited):- vizinhos(Node, NN),
+%                 enqueue([Node],Visited,F3),
+%                 sanitize(NN,F3,SanitizedNN),
+%                 stackin(SanitizedNN,F1,F2),
+%                 search(F2,F3).
